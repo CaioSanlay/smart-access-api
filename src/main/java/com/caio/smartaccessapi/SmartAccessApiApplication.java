d2a4f4c5a9a22092acc3a31dd9cabb15385738d9
Caio Sanlay <caiosanlayaraujo@gmail.com>
2026-03-20 16:20:55 -0300

    chore: projeto inicial Spring Boot

---
+++ src/main/java/com/caio/smartaccessapi/SmartAccessApiApplication.java
@@ -0,0 +1,13 @@
+package com.caio.smartaccessapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAccessApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartAccessApiApplication.class, args);
+    }
+
+}
